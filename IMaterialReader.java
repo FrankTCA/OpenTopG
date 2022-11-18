ea88260402741859c09f39814e6dcd93cf119c3e
nekdu64 <56726796+nekdu64@users.noreply.github.com>
2022-11-18 16:40:39 +0100

    Create a new option IfBlockError
    IfBlockError is a defaultmaterial if Block isn't recognise.

---
+++ IMaterialReader.java
@@ -0,0 +1,13 @@
+package com.pg85.otg.interfaces;
+
+import com.pg85.otg.exceptions.InvalidConfigException;
+import com.pg85.otg.util.materials.LocalMaterialData;
+import com.pg85.otg.util.materials.LocalMaterialTag;
+
+public interface IMaterialReader {
+    LocalMaterialData readMaterial(String material) throws InvalidConfigException;
+
+    LocalMaterialData readMaterial(String material,LocalMaterialData defaultBlock, String bo3name) throws InvalidConfigException;
+
+    LocalMaterialTag readTag(String tag) throws InvalidConfigException;
+}
